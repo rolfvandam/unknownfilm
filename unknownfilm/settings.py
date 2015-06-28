620dfdb4b6ebb4a5173bb38250778f08576e47c1
rolf <rolfvandam1@gmail.com>
2015-06-28 17:16:37 +0200

    fix

--- unknownfilm/settings.py
+++ unknownfilm/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+import private
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -76,8 +78,10 @@ WSGI_APPLICATION = 'unknownfilm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unknownfilm',
+        'USER': private.MYSQL_USER,
+        'PASSWORD': private.MYSQL_PASSWORD
     }
 }
 
